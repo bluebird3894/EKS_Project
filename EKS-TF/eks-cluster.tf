b1439f7b1fc4f5c4345946bd5ce665eace6258c6
Kamlesh <adekamlesh@outlook.com>
2026-02-08 22:14:09 +0530

    Upgrade EKS version to 1.30 for provider compatibility

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -1,12 +1,11 @@
+# Modified Reddit-Project/EKS-TF/eks-cluster.tf
 resource "aws_eks_cluster" "eks-cluster" {
-  name     = var.cluster-name
+  name     = var.eks_cluster_name
   role_arn = aws_iam_role.EKSClusterRole.arn
+  version  = "1.30" # Updated from 1.28 to meet Auto Mode requirements
+
   vpc_config {
     subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
     security_group_ids = [aws_security_group.eks_sg.id]
   }
-
-  version = 1.28
-
-  depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
 }
