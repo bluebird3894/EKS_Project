facef11b196d4f8b00644de8a37cd89f2d4bd29e
Kamlesh <adekamlesh@outlook.com>
2026-02-08 22:19:15 +0530

    Upgrade EKS version to 1.30 for provider compatibility-2

--- EKS-TF/eks-cluster.tf
+++ EKS-TF/eks-cluster.tf
@@ -1,12 +1,11 @@
+# Modified Reddit-Project/EKS-TF/eks-cluster.tf
 resource "aws_eks_cluster" "eks-cluster" {
-  name     = var.cluster-name
+  name     = var.cluster-name # Changed from var.eks_cluster_name to match variables.tf
   role_arn = aws_iam_role.EKSClusterRole.arn
+  version  = "1.30"
+
   vpc_config {
     subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
     security_group_ids = [aws_security_group.eks_sg.id]
   }
-
-  version = 1.30
-
-  depends_on = [aws_iam_role_policy_attachment.AmazonEKSClusterPolicy]
 }
